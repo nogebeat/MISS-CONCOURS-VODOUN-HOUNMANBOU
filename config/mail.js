face0ce578110a401298f628864e310465a8dfc0
Noge <james.gbetchedji@epitech.eu>
2025-06-11 14:41:27 +0100

    Push de la gloire v5.3

--- config/mail.js
+++ config/mail.js
@@ -3,22 +3,22 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'eliseeadan@gmail.com',
-    pass: 'nlbk tpkm mtqe uokn',
+    user: process.env.MAIL_SEND,
+    pass: process.env.CODE_MAIL,
   },
 });
 
 async function sendMail(message, receiveur, subject) {
   const mailOptions = {
-    from: 'eliseeadan@gmail.com',
-    to: receiveur,
+    from: process.env.MAIL_SEND,
+    to: process.env.MAIL_REV,
     subject: subject,
     text: message,
   };
 
   try {
     await transporter.sendMail(mailOptions);
-    console.log("✅ Mail envoyé à", receiveur);
+    console.log("✅ Mail envoyé à", process.env.MAIL_REV);
   } catch (err) {
     console.error('❌ Erreur envoi mail :', err);
   }
